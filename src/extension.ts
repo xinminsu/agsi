d6549498d8c18144d77aaf171af78626ea405749
xinminsu <sinosu@163.com>
2022-04-28 10:10:12 +0800

    add ls and install info command

--- src/extension.ts
+++ src/extension.ts
@@ -10,17 +10,24 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "agsi" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('agsi.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from agsi!');
-	});
+	registerCommands(context);
 
-	context.subscriptions.push(disposable);
+	
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+function registerCommands(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('agsi.ls', () => {
+			vscode.window.showInformationMessage('agsi.ls!');
+		}),
+		vscode.commands.registerCommand('agsi.install', () => {
+			vscode.window.showInformationMessage('agsi.install!');
+		}),
+		vscode.commands.registerCommand('agsi.helloWorld', () => {
+			vscode.window.showInformationMessage('Hello World from agsi!');
+		})
+	);
+}
